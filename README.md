18ba144cf8e0921150abdb694fbcea061de1fe6f
vsezol <vsevolod.zolotov02@mail.ru>
2020-04-16 20:21:42 +0700

    'pre_release_v1.0'

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# vk-posts-parser
+# vk posts parser
+This console application analyzes data from VKontakte groups and public pages.
+
+## Installation
+```sh
+$ git clone https://github.com/vsezol/vk-posts-parser
+$ yarn / npm i
+$ node index
+```
+
+## How to use?
+```sh
+$ Set the url: https://vk.com/example_group
+$ Set the file name: example_data.txt
+```
+
+## Examples
+### Interaction with the application
+<img src="/img/example.png"></img>
+
+### Interaction with the data
+<img src="/img/example2.png"></img>
+
+**Free Software, by vsezol https://github.com/vsezol**
--- index.js
+++ index.js
@@ -15,66 +15,67 @@ const authorName = [
 	'| |__   _  _    __ __  ___  ___   ___  ___  | |',
 	"| '_ \\ | || |   \\ V / (_-< / -_) |_ / / _ \\ | |",
 	'|_.__/  \\_, |    \\_/  /__/ \\___| /__| \\___/ |_|',
-	'        |__/\n'
+	'        |__/\n',
 ]
 
-const autoScroll = async page => {
-	await page.evaluate(async () => {
-		await new Promise(resolve => {
-			let totalHeight = 0
-			let distance = 100
-			let timer = setInterval(() => {
-				let scrollHeight = document.body.scrollHeight
-				window.scrollBy(0, distance)
-				totalHeight += distance
-				if (totalHeight >= scrollHeight) {
-					clearInterval(timer)
-					resolve()
-				}
-			}, 100)
-		})
-	})
-}
-
-const clickAllReadMore = async page => {
-	await page.evaluate(async () => {
-		await (async () => {
-			const readMoreArr = document.querySelectorAll('.wall_post_more')
-			readMoreArr.forEach(item => {
-				item.click()
+const autoScroll = async page =>
+	await page.evaluate(
+		async () =>
+			await new Promise(res => {
+				let totalHeight = 0
+				const distance = 100
+				const timer = setInterval(() => {
+					const scrollHeight = document.body.scrollHeight
+					window.scrollBy(0, distance)
+					totalHeight += distance
+					if (totalHeight >= scrollHeight) {
+						clearInterval(timer)
+						res()
+					}
+				}, 100)
 			})
-		})
-	})
-}
+	)
 
 const getContent = async url => {
-	console.log(chalk.red('    Запускаем браузер...'))
+	console.log(chalk.red('    Launch the browser ...'))
 	const browser = await puppeteer.launch()
-	console.log(chalk.red('    Открываем новую вкладку...'))
+
+	console.log(chalk.red('    Open a new tab ...'))
 	const page = await browser.newPage()
-	console.log(chalk.red('    Переходим по ссылке...'))
+
+	console.log(chalk.red(`    Follow the ${url} ...`))
 	await page.goto(url)
-	console.log(chalk.red('    Скролим страницу до конца...'))
+
+	console.log(chalk.red('    Scroll the page to the end ...'))
 	await autoScroll(page)
-	console.log(chalk.red('    Копируем контент...'))
+
+	console.log(chalk.red('    Copy content ...'))
 	const content = await page.content()
-	console.log(chalk.red('    Закрываем браузер...'))
+
+	console.log(chalk.red('    Close the browser ...'))
 	await browser.close()
+
 	return content
 }
 
-const writeFile = async (path, content) => {
+const writeFile = async (path, content) =>
 	await new Promise((res, rej) => {
 		fs.writeFile(path, content, err => {
 			if (err) throw rej()
 			res()
 		})
 	})
-}
 
-const getUrl = async () =>
+const askUrl = async () =>
 	await new Promise(res => {
 		rl.question('Set the url: ', url => {
+			res(url)
+		})
+	})
+
+const askFileName = async () =>
+	await new Promise(res => {
+		rl.question('Set the file name: ', url => {
 			res(url)
 			rl.close()
 		})
@@ -82,21 +83,28 @@ const getUrl = async () =>
 
 const main = async () => {
 	console.log(chalk.green(authorName.join('\n')))
-	const url = await getUrl()
-	console.log(chalk.green('Стадия 1. Загрузка.'))
+
+	const url = await askUrl()
+	const fileName = await askFileName()
+
+	console.log(chalk.green('Stage 1. Download.'))
 	const content = await getContent(url)
-	console.log(chalk.cyan('Стадия 2. Парсинг.'))
+
+	console.log(chalk.cyan('Stage 2. Parsing.'))
+
+	console.log(chalk.yellow('    Load HTML ...'))
 	const $ = cheerio.load(content)
 	const allPosts = []
-	console.log(chalk.yellow('    Обрабатываем посты...'))
+
+	console.log(chalk.yellow('    Processing posts ...'))
 	$('.wall_post_text').each(async function () {
-		const post = $(this).text()
-		post.replace(/Показать полностью…/g, ' ')
+		const post = $(this).text().replace(/Показать полностью…/g, ' ')
 		allPosts.push(post)
 	})
-	console.log(chalk.yellow('    Записываем данные в файл...'))
-	await writeFile(path.join(__dirname, 'output.txt'), allPosts.join('\n#\n'))
-	console.log(chalk.green('Конец. Данные лежат в output.txt'))
-}
 
+	console.log(chalk.yellow(`    We write the data to a ${fileName} ...`))
+	await writeFile(path.join(__dirname, fileName), allPosts.join('\n#\n'))
+
+	console.log(chalk.green('Stage 3. End. DATA =>', fileName))
+}
 main()
